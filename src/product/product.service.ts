acc332b85363b7c02674477ab58c09aa1267e5e0
Abdulaziz20007 <lil.gangsser@gmail.com>
2025-05-16 18:58:51 +0500

    done

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-<<<<<<< HEAD
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
@@ -35,28 +34,5 @@ export class ProductService {
     return this.prisma.product.delete({
       where: { id },
     });
-=======
-
-@Injectable()
-export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
-  }
-
-  findAll() {
-    return `This action returns all product`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
-  }
-
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} product`;
->>>>>>> b20a6394698a42480cfe66dd0444c030c89c2dbd
   }
 }
