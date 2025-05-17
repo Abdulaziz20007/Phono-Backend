f5326e168b1b55984579cfaf4bed04ef972f0923
Abdulaziz20007 <lil.gangsser@gmail.com>
2025-05-17 22:05:11 +0500

    done

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,3 +1,15 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
-    
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  surname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phone: string;
 }
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,26 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { IsString } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
+  @IsOptional()
+  avatar?: string;
+
+  @IsString()
+  @IsOptional()
+  refresh_token?: string;
+
+  @IsNumber()
+  @IsOptional()
+  balance?: number;
+
+  @IsNumber()
+  @IsOptional()
+  otp_id?: number;
+
+  @IsNumber()
+  @IsOptional()
+  currency_id?: number;
+}
