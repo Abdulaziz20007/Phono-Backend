55c983f6477c7416133eebbddf93e1c4579d7932
Abdulaziz20007 <lil.gangsser@gmail.com>
2025-05-29 12:02:27 +0500

    done

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
         'http://localhost:3001',
         'http://142.93.135.99:3000',
         'https://phono.ligma.uz',
+        'http://phono.ligma.uz',
       ],
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
       allowedHeaders: 'Content-Type,Accept,Authorization',
